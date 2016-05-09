18d99df34e1168191292d696978c1c8142865b61
Kelly Senna <jkellysenna@gmail.com>
2016-05-09 13:10:32 -0400

    Create outlets and actions for UI elements
    * Adds properties for itemTextField and tableView
    * Adds action when button is pressed (currently logs to console)

--- iTahDoodle/ViewController.swift
+++ iTahDoodle/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addButtonPressed(sender: UIButton) {
+        print("Add todo item: \(itemTextField)")
+    }
 
 }
 
